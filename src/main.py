55febea3bccdb2ba30632533c26b882718a99a46
Serafima <5580472sima@gmail.com>
2025-01-19 17:31:44 +0300

    Add multiply_numbers function

--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
 # main.py
+from utils import add_numbers, multiply_numbers
+
 if __name__ == "__main__":
     print("Hello from main.py!")
+    result_add = add_numbers(5, 3)
+    result_multiply = multiply_numbers(5, 3)
+    print(f"Add result: {result_add}")
+    print(f"Multiplн result: {result_multiply}")
